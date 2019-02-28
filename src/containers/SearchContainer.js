409599cb4eb2d5d8b06e732b7285c22f83cabfbc
hibow <utfnalin@gmail.com>
2019-02-28 00:40:55 -0600

    WIP for container

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,7 +2,23 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+
+  };
+};
+const mapDispatchToProps = (dispatch, ownpProps) => {
+  return {
+    handleSearchInputChange: (query) => dispatch(handleSearchChange(query))
+  };
+};
+
+var SearchContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
+//var SearchContainer = () => {};
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,7 +1,24 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
+const mapStateToProps = (state, ownProps) => {
+  const {video}
+  return {
+    video: state.current
+  };
+};
+const mapDispatchToProps = (dispatch, ownpProps) => {
+  return {
+    //handleVideoListEntryTitleClick: (video) => dispatch(changeVideo(video))
+  };
+};
+
+var VideoPlayerContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VideoPlayer);
+
+//var VideoPlayerContainer = () => {};
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
