ed6d140c19cb6c846355cc2dc144def9bc273e20
hibow <utfnalin@gmail.com>
2019-03-02 14:55:57 -0600

    redux tool added

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
 
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
+);
+//const store = createStore(rootReducer, enhancer);
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 const store = createStore(rootReducer, {currentVideo: null, videoList: []}, applyMiddleware(thunk));
 //currentVideo: null, videoList: []
